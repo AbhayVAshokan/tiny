028c72a4376188ab1d6245ebe111857d1b317910
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-12-07 11:26:13 +0530

    Upgrade to Next.js v16 and React v19

--- app/[shorturl]/page.tsx
+++ app/[shorturl]/page.tsx
@@ -2,12 +2,13 @@ import { notFound, permanentRedirect} from 'next/navigation';
 import { getLongUrl } from "@/app/actions";
 
 type ShortUrlProps = {
-  params: {
+  params: Promise<{
     shorturl: string;
-  };
+  }>;
 };
 
-export default async function ShortUrl({ params }: ShortUrlProps) {
+export default async function ShortUrl(props: ShortUrlProps) {
+  const params = await props.params;
   const { shorturl } = params;
 
   const url = await getLongUrl(shorturl);
